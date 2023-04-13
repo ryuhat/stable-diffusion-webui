a5e6dd6def7b7e5e7ef9b5935d997944dcfeb757
ryuhat <84193484+ryuhat@users.noreply.github.com>
2023-04-13 14:57:54 +0900

    init

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
 @echo off
+git pull
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--autolaunch --allow-code --opt-sdp-no-mem-attention --opt-channelslast
+@REM set COMMANDLINE_ARGS=
 
 call webui.bat
